05a4744541c0906cf3d21865427a474fd652c63b
MSB246 <msembulut@gmail.com>
2023-03-13 21:07:25 +0100

    Complete interpreter, might improve in the future

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # rabarber
+## A very simple ( and very bad ) Brainf\*ck interpreter
+
+## How did i create the name?
+Here's the creation process:
+
+1. Brainf\*ck
+2. Brainfreeze
+3. Brrr
+4. Rbrr
+5. Rabarber
+
+Believe it or not this is how I came up with the name.
--- main.c
+++ main.c
@@ -1,42 +1,136 @@
-enum Token {
+#include <stdint.h>
+#include <stdio.h>
+#include <malloc.h>
+
+#define DATA_AMOUNT 30000
+#define TOKEN_AMOUNT 128
+
+typedef enum Token {
+    Eof,
     IncP,
     DecP,
     IncD,
     DecD,
-    In,
     Out,
-};
+    In,
+    IfB,
+    IfE,
+} Token;
 
-struct Vm {
-    char data[3000];
+typedef struct Vm {
+    char *data;
     char *ops;
-    char *dptr;
-    int optr;
-};
+} Vm;
 
-void vm_next(struct Vm *vm) {
-    switch (vm->ops[vm->optr]) {
-        case (IncP):
-            ++vm->dptr;
-            break;
-        case (DecP):
-            --vm->dptr;
-            break;
-        case (IncD):
-            ++*vm->dptr;
-            break;
-        case (DecD):
-            --*vm->dptr;
-            break;
-        default:
-            break;
+Vm vm_new(char *tokens) {
+    char *data = (char*)malloc(sizeof(char)*DATA_AMOUNT);
+    char *ops = tokens;
+
+    return (Vm){ .data = data, .ops = ops };
+}
+
+void vm_eval(Vm vm) {
+    while (*vm.ops != Eof) {
+	switch (*vm.ops) {
+    	    case (IncP):
+    	        ++vm.data;
+    	        break;
+    	    case (DecP):
+    	        --vm.data;
+    	        break;
+    	    case (IncD):
+    	        ++*vm.data;
+    	        break;
+    	    case (DecD):
+    	        --*vm.data;
+    	        break;
+    	    case (Out):
+    	        putchar(*vm.data);
+    	        break;
+    	    case (Out):
+    	        *vm.data = getchar();
+    	        break;
+	    case (IfB):
+		if (!*vm.data) {
+		    uint32_t offset = 1;
+		    while (offset) {
+			vm.ops++;
+			if (*vm.ops == IfB) offset++; else if (*vm.ops == IfE) offset--;
+		    }
+		}
+		break;
+	    case (IfE):
+		if (*vm.data) {
+		    uint32_t offset = 1;
+		    while (offset) {
+			vm.ops--;
+			if (*vm.ops == IfE) offset++; else if (*vm.ops == IfB) offset--;
+		    }
+		}
+		break;
+    	    default:
+    	        break;
+    	}
+
+	vm.ops++;
     }
-    
-    vm->optr++;
+}
+
+char *scan(char *source) {
+    char *tokens = malloc(sizeof(Token)*TOKEN_AMOUNT);
+    uint32_t length = 0;
+    char *ptr = source;
+
+    while (*ptr != '\0') {
+	if (*ptr == ' ') {
+	    ptr++;
+	    continue;
+	}
+
+	switch (*ptr) {
+	    case ('>'):
+		tokens[length++] = IncP;
+		break;
+	    case ('<'):
+		tokens[length++] = DecP;
+		break;
+	    case ('+'):
+		tokens[length++] = IncD;
+		break;
+	    case ('-'):
+		tokens[length++] = DecD;
+		break;
+	    case ('.'):
+		tokens[length++] = Out;
+		break;
+	    case (','):
+		tokens[length++] = In;
+		break;
+	    case ('['):
+		tokens[length++] = IfB;
+		break;
+	    case (']'):
+		tokens[length++] = IfE;
+		break;
+	    default:
+		break;
+	}
+
+	ptr++;
+    }
+
+    tokens[length] = Eof;
+    return tokens;
 }
 
 int main() {
-    char *source = "";
+    char *source = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+    char *tokens = scan(source);
+    
+    Vm vm = vm_new(tokens);
+    vm_eval(vm);
 
+    free(vm.data);
+    free(tokens);
     return 0;
 }
