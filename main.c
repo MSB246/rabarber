5c698b43afcfa6f5f3fff7cdaac402d46845b1e9
MSB246 <msembulut@gmail.com>
2023-03-14 14:32:52 +0100

    Made some improvements and added support for file loading

--- main.c
+++ main.c
@@ -1,70 +1,49 @@
-#include <stdint.h>
 #include <stdio.h>
-#include <malloc.h>
 
+#define BF_CHR "><+-.,[]"
 #define DATA_AMOUNT 30000
-#define TOKEN_AMOUNT 128
+#define CHAR_AMOUNT 1024
 
-typedef enum Token {
-    Eof,
-    IncP,
-    DecP,
-    IncD,
-    DecD,
-    Out,
-    In,
-    IfB,
-    IfE,
-} Token;
+void eval(char *source) {
+    char data[DATA_AMOUNT] = {0};
+    char *optr = source;
+    char *dptr = data;
 
-typedef struct Vm {
-    char *data;
-    char *ops;
-} Vm;
-
-Vm vm_new(char *tokens) {
-    char *data = (char*)malloc(sizeof(char)*DATA_AMOUNT);
-    char *ops = tokens;
-
-    return (Vm){ .data = data, .ops = ops };
-}
-
-void vm_eval(Vm vm) {
-    while (*vm.ops != Eof) {
-	switch (*vm.ops) {
-    	    case (IncP):
-    	        ++vm.data;
+    while (*optr != EOF) {
+	switch (*optr) {
+    	    case ('>'):
+    	        ++dptr;
     	        break;
-    	    case (DecP):
-    	        --vm.data;
+    	    case ('<'):
+    	        --dptr;
     	        break;
-    	    case (IncD):
-    	        ++*vm.data;
+    	    case ('+'):
+    	        ++*dptr;
     	        break;
-    	    case (DecD):
-    	        --*vm.data;
+    	    case ('-'):
+    	        --*dptr;
     	        break;
-    	    case (Out):
-    	        putchar(*vm.data);
+    	    case ('.'):
+    	        putchar(*dptr);
     	        break;
-    	    case (Out):
-    	        *vm.data = getchar();
+    	    case (','):
+    	        *dptr = getchar();
     	        break;
-	    case (IfB):
-		if (!*vm.data) {
-		    uint32_t offset = 1;
+	    case ('['):
+		if (!*dptr) {
+		    int offset = 1;
 		    while (offset) {
-			vm.ops++;
-			if (*vm.ops == IfB) offset++; else if (*vm.ops == IfE) offset--;
+			optr++;
+			if (*optr == '[') offset++; else if (*optr == ']') offset--;
 		    }
 		}
 		break;
-	    case (IfE):
-		if (*vm.data) {
-		    uint32_t offset = 1;
+	    case (']'):
+		if (*dptr) {
+		    int offset = 1;
 		    while (offset) {
-			vm.ops--;
-			if (*vm.ops == IfE) offset++; else if (*vm.ops == IfB) offset--;
+			optr--;
+			if (*optr == ']') offset++; else if (*optr == '[') offset--;
 		    }
 		}
 		break;
@@ -72,65 +51,31 @@ void vm_eval(Vm vm) {
     	        break;
     	}
 
-	vm.ops++;
+	optr++;
     }
 }
 
-char *scan(char *source) {
-    char *tokens = malloc(sizeof(Token)*TOKEN_AMOUNT);
-    uint32_t length = 0;
-    char *ptr = source;
-
-    while (*ptr != '\0') {
-	if (*ptr == ' ') {
-	    ptr++;
-	    continue;
-	}
-
-	switch (*ptr) {
-	    case ('>'):
-		tokens[length++] = IncP;
-		break;
-	    case ('<'):
-		tokens[length++] = DecP;
-		break;
-	    case ('+'):
-		tokens[length++] = IncD;
-		break;
-	    case ('-'):
-		tokens[length++] = DecD;
-		break;
-	    case ('.'):
-		tokens[length++] = Out;
-		break;
-	    case (','):
-		tokens[length++] = In;
-		break;
-	    case ('['):
-		tokens[length++] = IfB;
-		break;
-	    case (']'):
-		tokens[length++] = IfE;
-		break;
-	    default:
-		break;
-	}
-
-	ptr++;
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        printf("Not enough arguments!\n");
+        return 1;
+    } else if (argc > 2) {
+        printf("Too many arguments!\n");
+        return 1;
     }
 
-    tokens[length] = Eof;
-    return tokens;
-}
+    FILE *source = fopen(argv[1], "r");
+    if (!source) {
+	printf("Couldn't open file: \"%s\"\n", argv[1]);
+	fclose(source);
+	return 1;
+    }
 
-int main() {
-    char *source = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
-    char *tokens = scan(source);
-    
-    Vm vm = vm_new(tokens);
-    vm_eval(vm);
+    char chars[CHAR_AMOUNT] = {0};
+    size_t length = fread(chars, sizeof(char), CHAR_AMOUNT, source);
+    chars[length] = EOF;
+    eval(chars);
 
-    free(vm.data);
-    free(tokens);
+    fclose(source);
     return 0;
 }
